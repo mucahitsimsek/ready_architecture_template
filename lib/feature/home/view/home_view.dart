dbe9d8c9b2222a7ffcbf3b442d8c54fc3349b85c
mucahit <mucahitsmsk74@gmail.com>
2024-01-26 21:17:18 +0300

    CustomNetworkImage addded on 'module/common'

--- lib/feature/home/view/home_view.dart
+++ lib/feature/home/view/home_view.dart
@@ -5,6 +5,7 @@ import 'package:architecture_template/product/init/language/locale_keys.g.dart';
 import 'package:architecture_template/product/init/product_localization.dart';
 import 'package:architecture_template/product/utility/constants/enums/locales.dart';
 import 'package:auto_route/annotations.dart';
+import 'package:common/common.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:gen/gen.dart';
@@ -53,6 +54,9 @@ class _HomeViewState extends State<HomeView> {
               ).tr(
                 args: ['Naber'],
               ),
+            ),
+            const CustomNetworkImage(
+              imageUrl: 'https://picsum.photos/250?image=9',
             ),
           ],
         ),
