baf05fcfe7d28d6f154dcba8ffb8069462610a0e
mucahit <mucahitsmsk74@gmail.com>
2024-01-26 20:03:13 +0300

    Auto route package added

--- lib/feature/home/view/home_view.dart
+++ lib/feature/home/view/home_view.dart
@@ -4,13 +4,15 @@ import 'package:architecture_template/product/init/config/app_environment.dart';
 import 'package:architecture_template/product/init/language/locale_keys.g.dart';
 import 'package:architecture_template/product/init/product_localization.dart';
 import 'package:architecture_template/product/utility/constants/enums/locales.dart';
+import 'package:auto_route/annotations.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:gen/gen.dart';
 
 part 'widget/home_app_bar.dart';
 
-class HomeView extends StatefulWidget {
+@RoutePage(name: 'HomeRoute')
+final class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 // ignore_for_file: public_member_api_docs
 
-import 'package:architecture_template/feature/home/view/home_view.dart';
 import 'package:architecture_template/product/init/app_initialize.dart';
 import 'package:architecture_template/product/init/product_localization.dart';
 import 'package:architecture_template/product/init/theme/custom_dark_theme.dart';
 import 'package:architecture_template/product/init/theme/custom_light_theme.dart';
+import 'package:architecture_template/product/navigation/app_router.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
@@ -17,12 +17,15 @@ void main() async {
   );
 }
 
-class _MyApp extends StatelessWidget {
+final class _MyApp extends StatelessWidget {
   const _MyApp();
+
+  static final _appRotuer = AppRouter();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: _appRotuer.config(),
       supportedLocales: context.supportedLocales,
       localizationsDelegates: context.localizationDelegates,
       locale: context.locale,
@@ -30,7 +33,6 @@ class _MyApp extends StatelessWidget {
       theme: CustomLightTheme().themeData,
       darkTheme: CustomDarkTheme().themeData,
       themeMode: ThemeMode.light,
-      home: const HomeView(),
     );
   }
 }
