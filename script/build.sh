951756e93ce6b49452baee01d94ce35265a9c747
mucahit <mucahitsmsk74@gmail.com>
2024-01-26 19:31:52 +0300

    Script created

--- script/build.sh
+++ script/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-flutter pub run build_runner build --delete-conflicting-outputs
-flutter pub run build_runner build 
-
-
+if [ "$1" = "force" ]
+then
+    flutter pub run build_runner build  --delete-conflicting-outputs
+else
+    flutter pub run build_runner build
+fi
